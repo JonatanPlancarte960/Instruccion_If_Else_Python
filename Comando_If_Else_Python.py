13be5140922bb9366c1062ad42b36bef161eeaf7
Jonatan Plancarte <jonatanplancarte960is@gmail.com>
2023-03-21 12:51:07 -0600

    Commit del Archivo README.md y IF ELSE en python

---
+++ Comando_If_Else_Python.py
@@ -0,0 +1,5 @@
+tamaño_objeto = 10
+if tamaño_objeto > 5:
+    print('Necesitamos Vigilar Ese Objeto')
+else: 
+    print('El Objeto no Representa una Amenaza')
---
+++ README.md
@@ -0,0 +1,2 @@
+# Uso de la instruccion If y Else en Python
+## El Texto de las condiciones se puede modificar a las necesidades del proyecto
